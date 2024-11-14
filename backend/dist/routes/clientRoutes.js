cc3098e91c72e91932305b34ea8d0c17a063cb37
Eliezer Tonkelski da Silva <elitonkelski@gmail.com>
2024-11-14 10:52:40 -0400

    Delete backend directory

--- backend/dist/routes/clientRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const clientController_1 = require("../controllers/clientController");
-const router = (0, express_1.Router)();
-// Defina a rota de criação de cliente
-router.post('/clients', clientController_1.createClient);
-exports.default = router;
